4f096672e4b84fef1b01dac954d67868285ee2a4
Alessandro Cecilio <alessandro.m.cecilio@gmail.com>
2026-06-02 18:06:22 -0300

    modificação da porta de conexão do mysql

--- www/conexao.php
+++ www/conexao.php
@@ -1,10 +1,10 @@
 <?php
     //abrir banco de dados:
-    $host_bd = "mysql";
+    $host_bd = "localhost";
     $login_bd = "root";
-    $password_bd = "xxxxxxx";
-    $nome_bd = "mediagenda";
-    $port = 3306;
+    $password_bd = "";
+    $nome_bd = "labdbprog2";
+    $port = 3307;
 
     $conexao_bd = mysqli_connect($host_bd, $login_bd, $password_bd,$nome_bd, $port);
 
